8ca3dfb7f2854a13c2e4d5c0e5f1b3f3526178cd
Aryaveer Sharma <24csu306@ncuindia.edu>
2025-02-11 10:08:32 +0530

    Q8.cpp

---
+++ Q8.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string fullName;
+    int age;
+
+    cout << "Enter your full name: ";
+    getline(cin, fullName);
+
+    cout << "Enter your age: ";
+    cin >> age;
+
+    int daysLived = age * 365;
+
+    cout << "Hello, " << fullName << "! You have lived approximately " << daysLived << " days.\n";
+
+    return 0;
+}
