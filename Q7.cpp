e81b3162f39e530b07d897599e2e4a888fefb9d2
Aryaveer Sharma <24csu306@ncuindia.edu>
2025-02-11 10:04:46 +0530

    Q7.cpp

---
+++ Q7.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+
+    int temp = a;
+    a = b;
+    b = temp;
+
+    cout << "After swapping (using temporary variable): a = " << a << ", b = " << b << endl;
+
+    a = a + b;  
+    b = a - b;  
+    a = a - b;  
+
+    cout << "After swapping (without temporary variable): a = " << a << ", b = " << b << endl;
+
+    return 0;
+}
