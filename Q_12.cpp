4635346d9ea68d60dae6610ae29c1b9cd62f2719
Aryaveer Sharma <24csu306@ncuindia.edu>
2025-02-18 10:37:41 +0530

    Update Q_12.cpp

--- Q_12.cpp
+++ Q_12.cpp
@@ -1,53 +1,36 @@
 #include <iostream>
-#include <cmath> 
-
+#include <cmath>
 using namespace std;
 
-int main() {
-    double a, b, c;
+int main() 
+{
+    double a,b,c;
 
+    cout<<"Enter the coefficient of a,b,c: "<<endl;
+    cin>>a>>b>>c;
     
-    cout << "Enter the coefficients a, b, and c: ";
-    cin >> a >> b >> c;
-
-    
-    if (a == 0) {
-        if (b != 0) {
-            
-            double root = -c / b;
-            cout << "This is a linear equation. The root is: " << root << endl;
-        } else {
-            
-            if (c == 0)
-                cout << "This equation has infinite solutions." << endl;
-            else
-                cout << "This equation has no solution!" << endl;
-        }
-    } else {
-        
-        double discriminant = b * b - 4 * a * c;
-
-        if (discriminant > 0) {
-            
-            double root1 = (-b + sqrt(discriminant)) / (2 * a);
-            double root2 = (-b - sqrt(discriminant)) / (2 * a);
-            cout << "The roots are real and different:" << endl;
-            cout << "Root 1 = " << root1 << endl;
-            cout << "Root 2 = " << root2 << endl;
-        } else if (discriminant == 0) {
-            
-            double root = -b / (2 * a);
-            cout << "The roots are real and equal:" << endl;
-            cout << "Root = " << root << endl;
-        } else {
-            
-            double realPart = -b / (2 * a);
-            double imaginaryPart = sqrt(-discriminant) / (2 * a);
-            cout << "The roots are complex:" << endl;
-            cout << "Root 1 = " << realPart << " + " << imaginaryPart << "i" << endl;
-            cout << "Root 2 = " << realPart << " - " << imaginaryPart << "i" << endl;
-        }
+    if(a==0) {
+        cout<<"This is not a linear equation"<<endl;
+        return 0;
     }
 
+    double discriminant = b*b - 4*a*c;
+
+    if(discriminant>0) {
+        double root1 = (-b+sqrt(discriminant))/(2*a);
+        double root2 = (-b-sqrt(discriminant))/(2*a);
+        cout<<"The roots are real and distinct : "<< root1 << "and " << root2 << endl;
+    }
+    else if (discriminant==0) {
+        double root = -b/(2*a);
+        cout<<"The roots are real and equal: "<< root << endl;
+    }
+    else {
+        double realPart = -b/(2*a);
+        double imaginaryPart = sqrt(-discriminant)/(2*a);
+        cout<<"The roots are complex : "<< realPart << "+" << imaginaryPart << "i and " << realPart << "-" << imaginaryPart << "i" << endl;
+
+    }
     return 0;
 }
+
