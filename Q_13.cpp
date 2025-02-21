ac70a730e6d62c9eed7d8023065ae12517f57106
Aryaveer Sharma <24csu306@ncuindia.edu>
2025-02-21 15:41:36 +0530

    Update Q_13.cpp

--- Q_13.cpp
+++ Q_13.cpp
@@ -1,68 +1,62 @@
 #include <iostream>
-#include <iomanip>
-
 using namespace std;
 
-int main() {
-    double balance = 1000.00; 
-    int choice;
+int main () {
     double amount;
+    double balance;
+    int choice;
 
     do {
-
-        cout << "\n===== ATM Menu =====\n";
-        cout << "1. Balance Inquiry\n";
-        cout << "2. Deposit\n";
-        cout << "3. Withdrawal\n";
-        cout << "4. Exit\n";
-        cout << "====================\n";
-        cout << "Enter your choice: ";
-        cin >> choice;
+        cout<< "\n ATM MACHINE";
+        cout<< "\n 1. Deposit";
+        cout<< "\n 2. Withdraw";
+        cout<< "\n 3. Check Balance";
+        cout<< "\n 4. Exit";
+        cout<< "\n Enter your choice ";
+        cin>> choice;
 
         switch (choice) {
-            case 1:
-                cout << fixed << setprecision(2);
-                cout << "Your current balance is: " << balance << "\n";
+            case 1: 
+                cout<< "\n Enter amount to deposit : $";
+                cin>>amount;
+                if (amount>0) {
+                    balance = balance + amount;
+                    cout<<"/n Deposit successful! Your new balance is : $"<<balance<<endl;
+                }
+                else {
+                    cout<<"Invalid deposit amount."<<endl;
+                }
                 break;
-
+            
             case 2:
-                cout << "Enter the amount to deposit: ";
-                cin >> amount;
-
-                if (amount > 0) {
-                    balance += amount;
-                    cout << "$" << amount << " deposited successfully.\n";
-                    cout << "Updated balance: " << balance << "\n";
-                } else {
-                    cout << "Invalid amount. Please enter a positive value.\n";
+                cout<<"\n Enter anount to withdraw: $";
+                cin>>amount;
+                if(amount>0 && amount<=balance) {
+                    balance -= amount;
+                    cout<<"\n Withdrawn successfully! Your new balance is : $" << balance << endl;
+                }
+                else if(amount > balance) {
+                    cout<< "\n Insufficient balance";
+                }
+                else {
+                    cout<<"\n Invalid withdrawal amount."<<endl;
                 }
                 break;
 
             case 3:
-                cout << "Enter the amount to withdraw: $";
-                cin >> amount;
-
-                if (amount > 0) {
-                    if (amount <= balance) {
-                        balance -= amount;
-                        cout << "$" << amount << " withdrawn successfully.\n";
-                        cout << "Updated balance: " << balance << "\n";
-                    } else {
-                        cout << "Insufficient balance.\n";
-                    }
-                } else {
-                    cout << "Invalid amount. Please enter a positive value.\n";
-                }
+                cout<<"\n Your current balance is : $ "<< balance << endl;
                 break;
-
-            case 4:
-                cout << "Thank you for using the ATM. Goodbye!\n";
+            
+            case 4: 
+                cout <<"\n Thank you for using the ATM. Goodbye !!" << endl;
                 break;
-
-            default:
-                cout << "Invalid choice. Please try again.\n";
+            
+            default: 
+                cout<< "\n Invalid choice. Please try again."<< endl;
         }
-    } while (choice != 4);
+
+    }
+    while (choice !=4);
 
     return 0;
 }
