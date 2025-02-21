1041fe050de2642747e42a8fe129cfeac7317ac6
Aryaveer Sharma <24csu306@ncuindia.edu>
2025-02-21 15:52:32 +0530

    Update Q_14.cpp

--- Q_14.cpp
+++ Q_14.cpp
@@ -2,21 +2,19 @@
 using namespace std;
 
 int main() {
-    double side1, side2, side3;
-    cout << "Enter the lengths of the three sides of the triangle: \n";
-    cin >> side1 >> side2 >> side3;
-    if (side1 + side2 > side3 && side1 + side3 > side2 && side2 + side3 > side1) {
-        cout << "The triangle is valid.\n";
+    double a,b,c;
 
-        if (side1 == side2 && side2 == side3) {
-            cout << "It is an equilateral triangle.\n";
-        } else if (side1 == side2 || side1 == side3 || side2 == side3) {
-            cout << "It is an isosceles triangle.\n";
-        } else {
-            cout << "It is a scalene triangle.\n";
-        }
-    } else {
-        cout << "The triangle is not valid.\n";
+    cout<< "Enter the values of three sides a, b, c : "<<endl;
+    cin>>a>>b>>c;
+
+    if(a == b && b==c && a==c) {
+        cout<< "\n It is an equilateral triangle. ";
+    }
+    else if(a == b || b == c || a==c) {
+        cout<<"\n It is an isosceles triangle. ";
+    }
+    else {
+        cout<<"\n It is a scalene triangle. ";
     }
 
     return 0;
